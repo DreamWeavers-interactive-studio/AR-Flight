34d5936b5014d545f776eb005ca71a77aad98c92
Ricardo Alfonso Camayo Erazo <ricardo.alfonso.camayo@gmail.com>
2024-05-15 00:49:24 -0500

    fix enemy rotation

--- My project/Assets/Scripts/EnemyWaveManager.cs
+++ My project/Assets/Scripts/EnemyWaveManager.cs
@@ -29,20 +29,20 @@ public class EnemyWaveManager : MonoBehaviour
         for (int i = 0; i < waveData.numberOfEnemies; i++)
         {
             GameObject randomEnemyPrefab = waveData.enemyPrefabs[Random.Range(0, waveData.enemyPrefabs.Length)];
-
             Vector3 randomPosition = new Vector3(
-            Random.Range(-waveData.spawnArea.x, waveData.spawnArea.x),
-            Random.Range(-waveData.spawnArea.y, waveData.spawnArea.y),
-            Random.Range(-waveData.spawnArea.z, waveData.spawnArea.z)
+                Random.Range(-waveData.spawnArea.x, waveData.spawnArea.x),
+                Random.Range(-waveData.spawnArea.y, waveData.spawnArea.y),
+                Random.Range(-waveData.spawnArea.z, waveData.spawnArea.z)
             );
 
+            // Calcular la rotación para que el GameObject "SpawnEnemy" apunte al jugador
             Quaternion lookRotation = Quaternion.LookRotation(player.position - randomPosition);
-            lookRotation *= Quaternion.Euler(-90f, 0f, 0f);
 
-            float rotationDeviation = Random.Range(-10f, 10f);
-            lookRotation *= Quaternion.Euler(rotationDeviation, rotationDeviation, rotationDeviation);
+            // Invertir la dirección frontal del GameObject "SpawnEnemy"
+            lookRotation *= Quaternion.Euler(0f, 180f, 0f);
 
-            GameObject enemy = Instantiate(randomEnemyPrefab, randomPosition, lookRotation);
+            // Instanciar el prefab "SpawnEnemy" con la rotación correcta
+            GameObject spawnEnemy = Instantiate(randomEnemyPrefab, randomPosition, lookRotation);
         }
     }
 }
